178624994720bccd9315bbacad985ba447e2f439
neo333670 <neo333670@gmail.com>
2021-03-17 14:26:00 +0800

    i did it
    but Always new file on the github is more convinient

---
+++ Assets/Scripts/CarEntity.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CarEntity : MonoBehaviour
+{
+    public GameObject wheelFrontLeft;
+    public GameObject wheelFrontRight;
+    public GameObject wheelBackLeft;
+    public GameObject wheelBackRight;
+    //Car Steering
+    float m_FrontWheelAngle = 0;
+    const float WHEEL_ANGLE_LIMIT = 40f;
+    public float turnAngulurVelocity = 20f;
+    //Accelerate and deceleration
+    float m_Velocity = 0;
+    public float Velocity { get { return m_Velocity; } }
+    public float maxVelocity = 40f;
+    public float acceleration = 3f;
+    public float deceleration = 10f;
+
+    float m_DeltaMovement = 0;
+    float CarLength = 0.96f;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) { 
+            // Speed up
+            m_Velocity = Mathf.Min(maxVelocity, m_Velocity + Time.deltaTime * deceleration);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            m_Velocity = Mathf.Max(0, m_Velocity - Time.deltaTime * deceleration);
+        }
+
+        m_DeltaMovement = m_Velocity * Time.deltaTime;
+        //Update car transform 
+        this.transform.Translate(Vector3.right * m_DeltaMovement);
+        this.transform.Rotate(0, 0, 1/CarLength *
+            Mathf.Tan(Mathf.Deg2Rad *m_FrontWheelAngle) 
+            *m_DeltaMovement *Mathf.Rad2Deg);
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            m_FrontWheelAngle = Mathf.Clamp(
+                m_FrontWheelAngle + Time.deltaTime * turnAngulurVelocity,
+                -WHEEL_ANGLE_LIMIT,
+                WHEEL_ANGLE_LIMIT );
+
+            UpdateWheels();
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            m_FrontWheelAngle = Mathf.Clamp(
+               m_FrontWheelAngle - Time.deltaTime * turnAngulurVelocity,
+               -WHEEL_ANGLE_LIMIT,
+               WHEEL_ANGLE_LIMIT );
+
+            UpdateWheels();
+        }
+    }
+
+    void UpdateWheels() {
+        Vector3 localEulerAngles = new Vector3 (0f, 0f, m_FrontWheelAngle);
+        wheelFrontLeft.transform.localEulerAngles = localEulerAngles;
+        wheelFrontRight.transform.localEulerAngles = localEulerAngles;
+    }
+}
---
+++ Assets/Scripts/TracingCameraEntity.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TracingCameraEntity : MonoBehaviour
+{
+    public CarEntity targetObject;
+    public float tracingLateFactor = 1;
+    public float MOVING_THRESHOLD = 10f;
+
+    Camera m_camera;
+    float m_OrthographicSize;
+
+    void Start() {
+        m_camera = this.GetComponent <Camera> ();
+        m_OrthographicSize = m_camera.orthographicSize;
+    }
+
+    void LateUpdate () {
+        Vector2 deltaPos = this.transform.position - targetObject.transform.position ;
+
+        m_camera.orthographicSize = m_OrthographicSize + targetObject.Velocity * 0.2f;
+
+        if (deltaPos.magnitude > MOVING_THRESHOLD) {
+            deltaPos.Normalize();
+
+            Vector2 newPosition = new Vector2 
+                (targetObject.transform.position.x, targetObject.transform.position.y)
+                + deltaPos * MOVING_THRESHOLD *tracingLateFactor;
+
+            this.transform.position = new Vector3(newPosition.x, newPosition.y, this.transform.position.z);
+        }
+    }
+}
